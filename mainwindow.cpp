8dc456c17c09c57591e594f8e80454f160f98baf
fafi09 <fafi09@126.com>
2020-07-18 20:21:45 +0800

    v0.3样式表

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QCompleter>
 #include <QStringList>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,6 +17,21 @@ MainWindow::MainWindow(QWidget *parent)
     com->setCaseSensitivity(Qt::CaseSensitive);
 
     ui->lineEdit->setCompleter(com);
+
+    ui->label_2->setStyleSheet("QLabel{color:rgb(0.255,255);"
+                               "background-color:green}");
+
+    this->setStyleSheet("QMainWindow{border-image:url(:/new/prefix1/C:/Users/Administrator/Downloads/sky.jpg)} " );
+    ui->pushButton->setStyleSheet("QPushButton{border:2px outset green;"
+                                  "border-image:url(:/new/prefix1/C:/Users/Administrator/Downloads/6334/6334/k16.ico);}"
+                                  "QPushButton:hover{"
+                                  "border-image:url(:/new/prefix1/C:/Users/Administrator/Downloads/6334/6334/k15.ico);}"
+                                  "QPushButton:pressed{"
+                                  "border-image:url(:/new/prefix1/C:/Users/Administrator/Downloads/5724/5724/0.ico);}");
+    connect(ui->pushButton,&QPushButton::clicked,
+            [=](){
+        QMessageBox::about(this,"prompt","login");
+    });
 }
 
 MainWindow::~MainWindow()
